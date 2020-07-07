8c4dee74ecfd71c3b1e3a223d78b2aee388e5dda
George Samuel Mahrous <31563545+gsamueil@users.noreply.github.com>
2020-07-07 17:05:54 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,11 @@
 """People Counter."""
-"""
- Copyright (c) 2018 Intel Corporation.
- Permission is hereby granted, free of charge, to any person obtaining
- a copy of this software and associated documentation files (the
- "Software"), to deal in the Software without restriction, including
- without limitation the rights to use, copy, modify, merge, publish,
- distribute, sublicense, and/or sell copies of the Software, and to
- permit person to whom the Software is furnished to do so, subject to
- the following conditions:
- The above copyright notice and this permission notice shall be
- included in all copies or substantial portions of the Software.
- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
- EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
- MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
- NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
- LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
- OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
- WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-"""
 
+'''
+To run the model-
+python main.py -i resources/Pedestrian_Detect_2_1_1.mp4 -m person-detection-retail-0013/FP32/person-detection-retail-0013.xml -l 
+/opt/intel/openvino/deployment_tools/inference_engine/lib/intel64/libcpu_extension_sse4.so -d CPU -pt 0.6 | ffmpeg -v 
+warning -f rawvideo -pixel_format bgr24 -video_size 768x432 -framerate 24 -i - http://0.0.0.0:3004/fac.ffm
+'''
 
 import os
 import sys
@@ -32,14 +19,48 @@ import paho.mqtt.client as mqtt
 
 from argparse import ArgumentParser
 from inference import Network
-
-# MQTT server environment variables
+global initial_w, initial_h, prob_threshold
 HOSTNAME = socket.gethostname()
 IPADDRESS = socket.gethostbyname(HOSTNAME)
 MQTT_HOST = IPADDRESS
 MQTT_PORT = 3001
 MQTT_KEEPALIVE_INTERVAL = 60
 
+def ssd_out(frame, result):
+    """
+    Parse SSD output.
+    :param frame: frame from camera/video
+    :param result: list contains the data to parse ssd
+    :return: person count and frame
+    """
+    current_count = 0
+    for obj in result[0][0]:
+        # Draw bounding box for object when it's probability is more than
+        #  the specified threshold
+        if obj[2] > prob_threshold:
+            xmin = int(obj[3] * initial_w)
+            ymin = int(obj[4] * initial_h)
+            xmax = int(obj[5] * initial_w)
+            ymax = int(obj[6] * initial_h)
+            cv2.rectangle(frame, (xmin, ymin), (xmax, ymax), (0, 55, 255), 1)
+            current_count = current_count + 1
+    return frame, current_count
+
+def performance_counts(perf_count):
+    """
+    print information about layers of the model.
+    :param perf_count: Dictionary consists of status of the layers.
+    :return: None
+    """
+    print("{:<70} {:<15} {:<15} {:<15} {:<10}".format('name', 'layer_type',
+                                                      'exec_type', 'status',
+                                                      'real_time, us'))
+    for layer, stats in perf_count.items():
+        print("{:<70} {:<15} {:<15} {:<15} {:<10}".format(layer,
+                                                          stats['layer_type'],
+                                                          stats['exec_type'],
+                                                          stats['status'],
+                                                          stats['real_time']))
 
 def build_argparser():
     """
@@ -61,148 +82,155 @@ def build_argparser():
                              "CPU, GPU, FPGA or MYRIAD is acceptable. Sample "
                              "will look for a suitable plugin for device "
                              "specified (CPU by default)")
-    parser.add_argument("-pt", "--prob_thresh", type=float, default=0.5,
+    parser.add_argument("-pt", "--prob_threshold", type=float, default=0.5,
                         help="Probability threshold for detections filtering"
                         "(0.5 by default)")
     return parser
 
-
-def model_out(frame, result):
-    
-    current_count = 0
-    for obj in result[0][0]:
-        if obj[2] > prob_thresh:
-            xmin = int(obj[3] * initial_width)
-            ymin = int(obj[4] * initial_height)
-            xmax = int(obj[5] * initial_width)
-            ymax = int(obj[6] * initial_height)
-            cv2.rectangle(frame, (xmin, ymin), (xmax, ymax), (0, 55, 255), 1)
-            current_count = current_count + 1
-    return frame, current_count
-
 def connect_mqtt():
-    
     client = mqtt.Client()
     client.connect(MQTT_HOST, MQTT_PORT, MQTT_KEEPALIVE_INTERVAL)
-
+    
     return client
 
+
+def infer_on_stream(args, client):
+    """
+    Initialize the inference network, stream video to network,
+    and output stats and video.
+    :param args: Command line arguments parsed by `build_argparser()`
+    :param client: MQTT client
+    :return: None
+    """
     
-def main():
-
-    args = build_argparser().parse_args()
-     
-    client = connect_mqtt()
-
-    global initial_width, initial_height,prob_thresh=0.4
-
-        # Initialise the class
-    network = Network()
-    # Set Probability threshold for detections
-    if args.prob_thresh is None:
-        prob_thresh= args.prob_thresh
-    else:
-        prob_thresh=0.4
-            
-
-    image_mode = False
+    # Flag for the input image
+    single_image_mode = False
 
     cur_request_id = 0
-    end = 0
-    total = 0
-    start = 0
-
-    # Load the network to IE plugin to get shape of input layer
+    last_count = 0
+    total_count = 0
+    start_time = 0
+    # Initialise the class
+    infer_network = Network()
+    # Set Probability threshold for detections
+    prob_threshold = args.prob_threshold
     
-    n, c, h, w = network.load_model(args.model, args.device, 1, 1,
-                                          cur_request_id, args.cpu_extension)[1]
+    # Initialize the Inference Engine
+    infer_network = Network()
 
+    # Load the network model into the IE
+    n, c, h, w = infer_network.load_model(args.model, args.device, 1, 1,
+                                          cur_request_id, 
+                                          args.cpu_extension)[1]
     
+    
+    # Checks for live feed
     if args.input == 'CAM':
-        inp_stream = 0
+        input_stream = 0
 
     # Checks for input image
     elif args.input.endswith('.jpg') or args.input.endswith('.bmp') :
-        image_mode = True
-        inp_stream = args.input
+        single_image_mode = True
+        input_stream = args.input
 
     # Checks for video file
     else:
-        inp_stream = args.input
-       
-    capture = cv2.VideoCapture(inp_stream)
+        input_stream = args.input
+        assert os.path.isfile(args.input), "Specified input file doesn't exist"
 
-    if inp_stream:
-        capture.open(args.input)
+    cap = cv2.VideoCapture(input_stream)
 
-    
-    initial_width = capture.get(3)
-    initial_height = capture.get(4)
-    
-    while capture.isOpened():
-        flag, frame = capture.read()
+    if input_stream:
+        cap.open(args.input)
+
+    if not cap.isOpened():
+        log.error("ERROR! Unable to open video source")
+   # global initial_w, initial_h, prob_threshold
+    prob_threshold = args.prob_threshold
+    initial_w = cap.get(3)
+    initial_h = cap.get(4)
+
+    while cap.isOpened():
+        
+        #Reading the next frame
+        flag, frame = cap.read()
         if not flag:
             break
         key_pressed = cv2.waitKey(60)
-        # Start async inference
- 
+        
+        # Pre-process the frame
         image = cv2.resize(frame, (w, h))
+        
         # Change data layout from HWC to CHW
         image = image.transpose((2, 0, 1))
         image = image.reshape((n, c, h, w))
         
-        # Start asynchronous inference for specified request.
+        inf_start = time.time()
         
-        inference_start = time.time()
-        network.exec_net(cur_request_id, image)
-        # Wait for the result
-        
-        if network.wait(cur_request_id) == 0:
-            det_time = time.time() - inference_start
-            # Results of the output layer of the network
-        
-        result = network.get_output(cur_request_id)
-        frame, current_count = model_out(frame, result)
+        # Perform inference on the frame
+        infer_network.exec_net(cur_request_id, image)
+
+        if infer_network.wait(cur_request_id) == 0:
             
-        inf_time_message = "Inference time: {:.3f}ms"\
+            det_time = time.time() - inf_start
+            
+            result = infer_network.get_output(cur_request_id)
+            #if args.perf_counts:
+            perf_count = infer_network.performance_counter(cur_request_id)
+            #performance_counts(perf_count)
+
+            frame, current_count = ssd_out(frame, result)
+            
+            inf_time_message = "Inference time: {:.3f}ms"\
                                .format(det_time * 1000)
             
-        cv2.putText(frame, inf_time_message, (15, 15),
+            cv2.putText(frame, inf_time_message, (15, 15),
                         cv2.FONT_HERSHEY_COMPLEX, 0.5, (200, 10, 10), 1)
-
-            # When new person enters the video
             
-        if current_count > end:
-            start = time.time()
-            total+= current_count - end
-            client.publish("person", json.dumps({"total": total}))
+            # When new person enters the video
+            if current_count > last_count:
+                start_time = time.time()
+                total_count = total_count + current_count - last_count
+                client.publish("person", json.dumps({"total": total_count}))
 
             # Person duration in the video is calculated
-        if current_count < end:
-            duration = int(time.time() - start)
-               # Publish messages to the MQTT server
-            client.publish("person/duration",
-                              json.dumps({"duration": duration}))
+            if current_count < last_count:
+                duration = int(time.time() - start_time)
+                # Publish messages to the MQTT server
+                client.publish("person/duration", json.dumps({"duration": duration}))
 
-       
-        client.publish("person", json.dumps({"count": current_count}))
-        end = current_count    
-    
+            client.publish("person", json.dumps({"count": current_count}))
+            last_count = current_count
 
-        if key_pressed == 27:
-                break
+            ### current_count, total_count and duration to the MQTT server ###
+            ### Topic "person": keys of "count" and "total" ###
+            ### Topic "person/duration": key of "duration" ###
 
-        # Send frame to the ffmpeg server
         sys.stdout.buffer.write(frame)  
         sys.stdout.flush()
 
-        if image_mode:
+        if single_image_mode:
             cv2.imwrite('output_image.jpg', frame)
+            
+        #CLEAN UP!
+        #cap.release()
+        #cv2.destroyAllWindows()
+        #client.disconnect()
+
+def main():
+    """
+    Load the network and parse the output.
+    :return: None
+    """
     
-    capture.release()
-    cv2.destroyAllWindows()
-    client.disconnect()
-    network.clean()
+    
+    # Grab command line args
+    args = build_argparser().parse_args()
+    # Connect to the MQTT server
+    client = connect_mqtt()
+    # Perform inference on the input stream
+    infer_on_stream(args, client)
+
 
 if __name__ == '__main__':
     main()
